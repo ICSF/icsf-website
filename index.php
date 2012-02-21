22d8a419b7d71704f6300f88628ac331fd59dcba
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-02-21 22:02:37 +0000

    Added outline of index.php

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-Under Construction
-
+#!/usr/bin/php
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" dir="ltr">
+	<head>
+		<title>ICSF - Imperial College Science Fiction and Fantasy</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" href="/~bh308/new-icsf/resources/style.css" />
+	</head>
+	<body>
+	</body>
+</html>
