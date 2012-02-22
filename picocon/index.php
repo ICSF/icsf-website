65b6918b49a514efc6f71ec87009b24211bce1a8
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-02-22 10:21:52 +0000

    Add an initial version of the Picocon page
    This is still in the process of being stripped down to the bare
    essentials

---
+++ picocon/index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Picocon 30 - ICSF</title>
+		<link type="text/css" rel="stylesheet" href="/scc/icsf/resources/style.css" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<style>
+			table#authors, table {
+			  border-collapse:collapse;
+			  margin: 0 auto;
+			}
+			td, th {
+			  border: 1px solid black;
+			  padding: 1px 4px 1px 4px;
+			}
+			table#authors td {
+			  vertical-align: top;
+			}
+			table#authors td p {
+			  text-align: left;
+			}
+		</style>
+	</head>
+	<body>
+		<h1 style="text-align: center;">
+			<img class="left" src="/scc/icsf/resources/logo.png" style="height:
+			80px;" />
+			<img class="right" src="/scc/icsf/resources/logo.png" style="height:
+			80px; margin-bottom: 5px;" />
+			Picocon 30
+			<div class="clear" />
+		</h1>
+
+		<nav>
+			<a href="#goh">Guests</a>
+			<a href="#prices">Prices</a>
+			<a href="#directions">Directions</a>
+			<a href="#contact">Contact Us</a>
+			<hr />
+			<a href="/scc/icsf">ICSF Webiste</a>
+			<a href="/scc/icsf/history/picocon">Picocon's History</a>
+		</nav>
+
+		<p style="font-variant: small-caps; font-size: 16pt;">
+			Picocon 30 will be held on Saturday 16th February 2013,
+			in Imperial College Union.
+		</p>
+
+		<h2>What is Picocon?</h2>
+		<p>
+			Picocon is the annual one-day Science Fiction &amp; Fantasy convention
+			run by the Imperial College Science Fiction and Fantasy Society, ICSF.
+			It usually takes place on a Saturday in February, in Imperial
+			College's Student Union.
+			We try not to clash with other conventions around the same time.
+		</p>
+		<p>
+			It is a small (hence the name), affordable and convenient convention
+			for students and fans in or near London.
+			Registration opens at 10am, with the first scheduled events kicking
+			off at around 10:30.
+			The schedule concludes in the evening.
+
+			At a Picocon you will encounter:
+
+		    <ul>
+				<li>Guests of Honour doing talks and panels</li>
+				<li>The Destruction of Dodgy Merchandise, typically using liquid nitrogen and an enormous hammer. (You're welcome to bring along donations!)</li>
+				<li>Multiplayer LAN gaming</li>
+				<li>Stalls selling books, ICSF t-shirts, and other stuff</li>
+				<li>Quiz and silly games</li>
+				<li>Student Union bar</li>
+			</ul>
+		</p>
+
+		<a name="goh"><h2>Guests of Honour</h2></a>
+		<p>
+			This years guests of honour are yet to be announced.
+		</p>
+
+		<a name="prices"><h2>Prices</h2></a>
+
+		<p>
+			Entry for the day will cost (subject to change up to Feb 2013):
+			<ul>
+				<li><b>&#x00A3;5</b> for ICSF members;</li>
+				<li><b>&#x00A3;8</b> concessions (with valid ID) - students/DHSS/OAP/children;</li>
+				<li><b>&#x00A3;10</b> full price;</li>
+				<li><b>FREE</b> for past Guests of Honour.</li>
+			</ul>
+		</p>
+		<p>
+			Discounts for large parties (e.g. other University SF societies)
+			are potentially available 
+			(email the <a href="mailto:picocon@icsf.co.uk">Picocon Sofa</a>).
+		</p>
+		<p>
+			Please note that neither ICSF nor Union membership is required to attend Picocon;
+			all are welcome.
+		</p>
+
+		<a name="#directions"><h2>Directions to Picocon</h2></a>
+		<p>
+			Here's a general <a href="http://g.co/maps/7ahv7">map</a> of the
+			area, pointing at the entrance to Beit Quad (which houses the
+			Union) off Prince Consort Road.
+		</p>
+		<p>
+			The easiest way to get to Picocon is by tube; both Gloucester
+			Road Station and South Kensington Station are within easy reach;
+			for your conveinience, these maps shoe the routes we suggest from
+			both <a href="http://g.co/maps/ne7kr">Gloucester Road Station</a>
+			and from <a href="http://g.co/maps/akrjw">South Kensington Station</a>.
+		</p>
+
+		<p>
+			The buses C1, 9, 10, 49, 52, 70, 74, 345, 360, 414 and 452 all stop nearby.
+			Please look <a href="http://www.tfl.gov.uk/tfl/gettingaround/maps/buses/pdf/royalalberthall-12445.pdf">here</a>
+			for the bus stops around the Royal Albert Hall (which is <i>literally</i>
+			next door to the Union) or <a href="http://www.tfl.gov.uk/tfl/gettingaround/maps/buses/pdf/southkensington-2236.pdf">here</a>
+			for the bus stops around South Kensington Station.
+		</p>
+		<p>
+			<b>Important Advice:</b> If you get lost around South
+			Kensington trying to find Beit Quad, ask for directions to the
+			Royal Albert Hall, and once there, Beit Quad is the building
+			on the left of the RAH as you face it's main entrance
+			(if you arrive form the Hyde Park side, you will be at the
+			rear of the RAH).
+		</p>
+
+		<a name="contact"><h2>Contact Us</h2></a>
+		<p>
+			For more information, please contact <b>Maciej Matuszewski</b>,
+			the Picocon Sofa (as opposed to chair), at
+			<a href="mailto:picocon@icsf.org.uk">picocon@icsf.org.uk</a>.
+		</p>
+		<p>Our mail address is the following:</p>
+		<blockquote>
+			ICSF<br />
+			Student Activites Centre <br />
+			Imperial College Students' Union<br />
+			Beit Quad<br />
+			Prince Consort Road<br />
+			SW7 2BB London<br />
+			UK
+		</blockquote>
+
+		<footer>
+			<p>
+				The first Picocon was in 1984.
+				If you're curious, we have a record of <a href="/scc/icsf/history/">Picocons of years gone by</a>.
+			</p>
+			<p class="copyright">Imperial College Science Fiction Scoiety</p>
+		</footer>
+
+	</body>
+</html>
