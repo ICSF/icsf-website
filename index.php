e805ab5ca0de73fb590dadd458657d6918a0d49d
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-02-22 10:14:47 +0000

    Add intro from old website

--- index.php
+++ index.php
@@ -6,5 +6,46 @@
 		<link rel="stylesheet" type="text/css" href="/scc/icsf/resources/style.css" />
 	</head>
 	<body>
+		<h1>
+			ICSF
+			<div style="font-size: 0.3em; line-height: normal;">
+				Imperial College Science Fiction, Fantasy, and Gothic Horror Society
+			</div>
+			<div class="clear"></div>
+		</h1>
+
+		<img class="hang-right" src="http://union.ic.ac.uk/scc/icsf/ftpupload/icsfcam.jpeg"
+		style="width: 320px; height: 240px;" />
+
+		<p>
+			ICSF is the centre for science fiction, fantasy and horror fans at
+			Imperial College. We run a library of over 7000 books and 1000 DVDs
+			and videos from all corners of the genre. Whether your interest is
+			Pratchett's 'Discworld', 'The Sandman' graphic novels or 'Stargate'
+			this is an ideal way to indulge it! And a great place to pick up new
+			and exciting obsessions too!
+		</p>
+		<p>
+			The social side of the society is
+			equally active - the library is open every lunchtime on weekdays and
+			we have regular video showings (often weird or esoteric stuff you
+			should have seen but won't have). Barnights are on Thursdays, these
+			are often frequented by past members of the society. We also run
+			events like mass outings to the cinema and end of term dinners.
+		</p>
+		<p>
+			The
+			largest event of the year is Picocon, our one-day convention held in
+			February, which usually attracts around 200 attendees.
+		</p>
+		<p>
+			In all, being
+			a member is a lot of fun and a great way to make new friends who are
+			interested in SF or fantasy.
+			Interested?  We can be found in our purpose built library in the
+			West Wing Basement of Beit Quad (the Students Union building),
+			opposite Stoic TV. For more detailed instructions, see the
+			instructions here.
+		</p>
 	</body>
 </html>
