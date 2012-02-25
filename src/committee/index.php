24950ffcc249e961704f943aa97ef87dd456151b
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-02-25 17:35:46 +0000

    [COMMITTEE] Add the committee page

---
+++ src/committee/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" dir="ltr">
+	<head>
+		<title>ICSF - Imperial College Science Fiction and Fantasy</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" href="<!--SRVROOT-->/resources/style.css" />
+		<link href="http://fonts.googleapis.com/css?family=Lora|Alice" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<h1>
+			<img id="logo" src="<!--SRVROOT-->/resources/logo.png" />
+			<div id="logo-balance"></div>
+			ICSF
+			<div style="font-size: 0.5em; line-height: normal;">Committee</div>
+			<div class="clear"></div>
+		</h1>
+
+		<nav>
+			<a href="<!--SRVROOT-->/committee/">Committee</a>
+			<a href="<!--SRVROOT-->/committee/minutes/">Meetings</a>
+			<a href="#constitution">Constitution</a>
+			<hr />
+			<a href="<!--SRVROOT-->/">Home</a>
+			<a href="<!--SRVROOT-->/library/">Library</a>
+			<a href="<!--SRVROOT-->/picocon/">Picocon</a>
+		</nav>
+
+		<p>
+			The committee form the core of the society, and are as close to in
+			charge of the society as anyone can be; they have meetings, approve
+			budgets, and generally try and make sure things happen.
+			Liberating Daleks, cuddling plushie great old one, and wearing scary
+			t-shirts are all optional, but fun, pastimes of committee.
+		</p>
+
+		<h2>The Committee</h2>
+
+		<div class="columns">
+
+		<div class="no-break"><h3 style="margin-top: 0;">Chair Entity: ?
+		(<span class="sans"><a href="mailto:chair@icsf.org.uk">chair@icsf.org.uk</a></span>)</h3>
+		<p>
+			The buck stops here. Responsible for overseeing everything that's
+			happening, and doing it themselves if all else fails, which it
+			usually does.
+			The Chair also has to attend SCC meetings, be aware of what the
+			Treasurer and the Librarian are doing and delegate lots.
+		</p></div>
+
+		<div class="no-break"><h3>Chair of Vice: ?
+		(<span class="sans"><a href="mailto:vicechair@icsf.org.uk">vicechair@icsf.org.uk</a></span>)</h3>
+		<p>
+			Keeping the populace from rebelling against the committee by the
+			regular organisation of bar nights is the primary purpose of the
+			Chair of Vice, a long with any other events - such as the annual
+			trip to Hay-on-Wye, and cinema outings as things come out.
+		</p></div>
+
+		<div class="no-break"><h3>Secretary: ?
+		(<span class="sans"><a href="mailto:secretary@icsf.org.uk">secretary@icsf.org.uk</a></span>)</h3>
+		<p>
+			Responsible for taking, writing up, and occasionally editing
+			minutes of meetings. Sound easy? T'ain't.
+			For reasons unknown, they generally end up the only committee
+			member who knows what's happening and spend their time trying to
+			keep the rest of the committee on task.
+		</p></div>
+
+		<div class="no-break"><h3>Treasurer: ?
+		(<span class="sans"><a href="mailto:treasurer@icsf.org.uk">treasurer@icsf.org.uk</a></span>)</h3>
+		<p>
+			Deals with the cash, turns up to meetings and hopefully makes
+			calming noises along lines of "we have lots of money, don't worry".
+			An indispensable post, which sounds a lot easier than it is.
+		</p></div>
+
+		<div class="no-break"><h3>Librarian: ?
+		(<span class="sans"><a href="mailto:librarian@icsf.org.uk">librarian@icsf.org.uk</a></span>)</h3>
+		<p>
+			The librarian is responsible for the library and its bounteous contents.
+			Duties includes maintaining the database, organising book buys,
+			chasing bad people with overdue items!
+		</p></div>
+
+		<div class="no-break"><h3>Picocon Sofa: ?
+		(<span class="sans"><a href="mailto:picocon@icsf.org.uk">picocon@icsf.org.uk</a></span>)</h3>
+		<p>
+			Picocon is our annual one-day convention, which tends to be
+			stupendous fun for everyone attending, and horribly stressful for
+			the organisers.
+			The Sofa chairs the Picocon sub-committee, and must manage all
+			aspects of Picocon, whilst being directly responsible for all
+			external contacts.
+		</p></div>
+
+		<div class="no-break"><h3>Picocon Beanbag: ?</h3>
+		<p>
+			The vice-chair of the Picocon committee, the Picocon Beanbag is
+			responsible for the arrangement for Picocon that are internal to
+			the college.
+			This generally involves beating off other societies that want the
+			rooms we use with a metaphorical stick, whilst waiting to be
+			elevated to Picocon Sofa for next year.
+		</p></div>
+
+		<div class="no-break"><h3>Editor: ?
+		(<span class="sans"><a href="mailto:editor@icsf.org.uk">editor@icsf.org.uk</a></span>)</h3>
+		<p>
+			Produces the society's bi-monthly newsletter Wyrmtongue and
+			the annual Fanzine. Lives in continual hope of finding a keen
+			fresher to hive Wyrmtongue off to.
+		</p></div>
+
+		<div class="no-break"><h3>Publicity Officer: ?
+		(<span class="sans"><a href="mailto:publicity@icsf.org.uk">publicity@icsf.org.uk</a></span>)</h3>
+		<p>
+			Designs, prints and puts up the posters.
+			Also responsible for distributing Wyrmtongue (our newsletter) and
+			acquiring as much blu-tac as possible.
+			Gets grumbled at if no-one turns up to events.
+		</p></div>
+
+		<div class="no-break"><h3>Tech Priest: ?
+		(<span class="sans"><a href="mailto:techpriest@icsf.org.uk">techpriest@icsf.org.uk</a></span>)</h3>
+		<p>
+			The tech-priest is responsible for the upkeep of all hardware and
+			software in use by the society. This includes this website, the
+			library computers, and the library's audio/visual stack.
+		</p></div>
+		</div>
+
+		<a name="constitution"><h2>Committee Duties</h2></a>
+
+		<p>
+			The committee doesn't just get to do what they want (quite).
+			There are a number of documents that define what both the committee
+			and the members are expected to do, what they're allowed to do,
+			and what they can do to each other when they start failing.
+		</p>
+		<p>
+			For the most parts, these can be ignored by anyone with a
+			bureaucracy level of less than 34.
+		</p>
+		<ul>
+			<li><a href="/<!--SRVROOT-->/committee/constitution.pdf">ICSF's Constitution</a> (PDF)</li>
+			<li><a href="/<!--SRVROOT-->/committee/standing-orders.pdf">ICSF's Standing Orders</a> (PDF)</li>
+		</ul>
+
+		<footer>
+			<p class="copyright">Imperial College Science Fiction Society.
+			Please report issues to <a class="sans"
+			href="mailto:techpriest@icsf.org.uk">techpriest@icsf.org.uk</a></p>
+		</footer>
+	</body>
+</html>
+
