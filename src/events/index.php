90501e18718df871e5508253e3420040b99c0e44
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-02-26 15:45:36 +0000

    [STUBS] Stubs in events page

--- src/events/index.php
+++ src/events/index.php
@@ -1,37 +1,24 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" dir="ltr">
 	<head>
-		<!--include "stubs/headers.html" -->
+		<!--include "stubs/headers.html"-->
 		<title>Events - ICSF</title>
 	</head>
 	<body>
-		<h1>
-			<img id="logo" src="<!--SRVROOT-->/resources/logo.png" />
-			ICSF
-			<div style="font-size: 0.5em; line-height: normal;">
-				Upcoming Events
-			</div>
-			<div class="clear"></div>
-		</h1>
+		<!--include "stubs/h1-start.html"-->
+			Upcoming Events
+		<!--include "stubs/h1-end.html"-->
 
 		<nav>
-			<a href="<!--SRVROOT-->/">Home</a>
-			<a href="<!--SRVROOT-->/library/">Library</a>
-			<a href="<!--SRVROOT-->/events/">Events</a>
-			<a href="<!--SRVROOT-->/committee/">Committee</a>
-			<a href="<!--SRVROOT-->/picocon/">Picocon</a>
+			<!--include "stubs/nav-main.html"-->
 		</nav>
 
 		<iframe src="https://www.google.com/calendar/embed?src=icsflib%40gmail.com&amp;ctz=Europe/London&amp;showPrint=0&amp;showCalendars=0&amp;showTitle=0"
 			style="width: 100%; min-height: 600px; resize: both; overflow: hidden;
 			padding: 0.3em; box-sizing: border-box;
-			border: 2px #ccc solid;	background: white; border-radius: 5px;" />
+			border: 2px #ccc solid;	background: white; border-radius: 5px;"></iframe>
 
-		<footer>
-			<p class="copyright">Imperial College Science Fiction Society.
-			Please report issues to <a class="sans"
-			href="mailto:techpriest@icsf.org.uk">techpriest@icsf.org.uk</a></p>
-		</footer>
+		<!--include "stubs/footer.html"-->
 	</body>
 </html>
 
