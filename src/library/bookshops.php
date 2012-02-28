6e276608c1c5a85a32b74eee7f932d83ce11c685
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-02-28 11:06:25 +0000

    [LIB] Add the bookshop's map

---
+++ src/library/bookshops.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<!--include "stubs/headers.html"-->
+		<title>New Library Items - ICSF</title>
+	</head>
+	<body>
+
+		<!--include "stubs/h1-start.html"-->
+			Bookshops
+		<!--include "stubs/h1-end.html"-->
+
+		<nav>
+			<!--include "stubs/nav-main.html"-->
+			<hr/>
+			<!--include "stubs/nav-lib.html"-->
+		</nav>
+
+		<iframe style="width: 100%; margin: 0 0.5em 0.5em 0; min-height: 600px;
+			resize: both; border: 2px solid #aaa; border-radius: 5px;"
+			frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+			src="http://maps.google.co.uk/maps/ms?msa=0&amp;msid=200564695773655420843.000492596c49a25618890&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=51.50901,-0.152435&amp;spn=0.018697,0.068665&amp;z=14&amp;output=embed"
+		></iframe>
+		<br />
+		<small>
+			View <a href="http://maps.google.co.uk/maps/ms?msa=0&amp;msid=200564695773655420843.000492596c49a25618890&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=51.50901,-0.152435&amp;spn=0.018697,0.068665&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left">SciFi Bookshops, London</a> in a larger map
+		</small>
+
+		<!--include "stubs/footer.html"-->
+	</body>
+</html>
