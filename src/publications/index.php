5dbeaaba8c25f5d22ff637b0d88a8617ad89f5e2
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-03-01 12:20:48 +0000

    [PUBS] Add basis from publications

---
+++ src/publications/index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" dir="ltr">
+	<head>
+		<!--include "stubs/headers.html"-->
+		<title>Publications - ICSF</title>
+	</head>
+	<body>
+		<!--include "stubs/h1-start.html"-->
+			Publications
+		<!--include "stubs/h1-end.html"-->
+
+		<nav>
+			<!--include "stubs/nav-main.html"-->
+		</nav>
+
+		<h2>Fanzines</h2>
+		<p>Latest Fanzine: &lsquot;<a href="fanzines/danger_dreams/danger-dreams-at-work.pdf">Danger: Dreams at Work</a>&rsquot; (Dec 2009)</p>
+		<p>
+			The icsf Fanzines are (usually) annual magazines produced by
+			members of the society (fanzine=magazine produced by fans).
+			They have contained anything from fiction, artwork, and poems,
+			to reviews and essays of sf/fantasy-related topics.
+		</p>
+		<p>
+			As far as we can tell, the first fanzines of the Science Fiction
+			society at Imperial were called NME, and were probably
+			between 1979-1984.
+			There was a gap of 4 years between WHJC and The Waking Edge,
+			followed by several years of regular fanzines with
+			'Edge'-related titles.
+			From 1994 onwards, the fanzines have been named after a scientific
+			term, chosen by the Editor until 2003.
+		</p>
+		<p>
+			In 2004, the Fanzine either stopped being published, or simply
+			never made it to the website, until 2009, where the practice of
+			uploading the (print-quality) master PDFs began.
+		</p>
+
+		<h3>Submissions</h3>
+		<p>
+			Submissions for the fanzine are always welcome, and should be sent
+			to the editor at <a href="mailto:editor@icsf.org.uk">editor@icsf.org.uk</a>
+		</p>
+
+		<h3>Past Fanzines</h3>
+<table cellpadding="2" class="bodyLight">
+	<tr>
+		<th nowrap ="nowrap">Year</th>
+		<th>Fanzine</th>
+		<th nowrap ="nowrap">Editor(s)</th>
+	</tr>
+	<tr>
+		<td>1981-1982</td>
+		<td><a href="fanzines/nme3/nme3_cover.html">NME 3</a></td>
+		<td>Elise Pechersky</td>
+	</tr>
+	<tr>
+		<td>1984-ish?</td>
+		<td><a href="fanzines/section10/section10_cover.html">Section 10, the alternative S.F. Soc magazine</a></td>
+		<td>Guy Riddihough</td>
+	</tr>
+	<tr>
+		<td nowrap ="nowrap">October 1985</td>
+		<td><a href="fanzines/whjc/whjc_cover.html">WHJC</a></td>
+		<td>Dave Clements</td>
+	</tr>
+	<tr>
+		<td>1989/90</td>
+		<td><a href="fanzines/we/we_cover.html">The Waking Edge</a></td>
+		<td>Amanda Baker</td>
+	</tr>	
+	<tr>
+		<td>1990/91</td>
+		<td><a href="fanzines/the_edge/the_edge_cover.html">The Edge</a></td>
+		<td>Paul Davison</td>
+	</tr>	
+	<tr>
+		<td>October 1991</td>
+		<td><a href="fanzines/et1/et1_cover.html">Edge of Tomorrow</a></td>
+		<td>Alex McLintock</td>
+	</tr>
+	<tr>
+		<td>1992</td>
+		<td><a href="fanzines/et2/et2_cover.html">The Edge of Tomorrow II</a></td>
+		<td>Richard Yagodich</td>
+	</tr>
+	<tr>
+		<td>1993</td>
+		<td><a href="fanzines/xenomorph/xenomorph_cover.html">Xenomorph</a></td>
+		<td>jk</td>
+	</tr>
+	<tr>
+		<td>June 1994</td>
+		<td><a href="fanzines/arcs/arcs_cover.html">Arcs</a></td>
+		<td>Luke Gietzen and jk</td>
+	</tr>
+	<tr>
+		<td>June 1995</td>
+		<td><a href="fanzines/parsec/parsec_cover.html">Parsec</a></td>
+		<td nowrap ="nowrap">Stian "naSti" Ingebrethsen</td>
+	</tr>
+	<tr>
+		<td>June 1996</td>
+		<td><a href="fanzines/metathesis/metathesis_cover.html">Metathesis</a></td>
+		<td>Gidon 'Sandals' Moont</td>
+	</tr>
+	<tr>
+		<td>June 1997</td>
+		<td><a href="fanzines/paradigm/paradigm_cover.html">Paradigm</a></td>
+		<td>Michael 'karne' Wright</td>
+	</tr>
+	<tr>
+		<td>June 1998</td>
+		<td><a href="fanzines/synergy/synergy_cover.html">Synergy</a></td>
+		<td>Liz 'ShadowCat' Hodson</td>
+	</tr>
+	<tr>
+		<td>1999</td>
+		<td>The 1999 fanzine never happened, but the stories went into Wyrmtongue 4</td>
+		<td>Alex 'Bushbaby' Taylor</td>
+	</tr>
+	<tr>
+		<td>Feb 2000</td>
+		<td><a href="fanzines/axiom/axiom_cover.html">Axiom</a>	</td>
+		<td>Lloyd 'Hack' Kilford and <br />Lorna Robinson</td>
+	</tr>
+	<tr>
+		<td>2001</td>
+		<td>The 2001 fanzine never happened, but the stories went into Wyrmtongue 6</td>
+		<td>Michael 'karne' Wright</td>
+	</tr>
+	<tr>
+		<td>2002</td>
+		<td><a href="fanzines/perihelion/Perihelion_cover.html">Perihelion</a></td>
+		<td>Alex Crosse</td>
+	</tr>
+	<tr>
+		<td>2003</td>
+		<td><a href="fanzines/dream_twister/The Dream Twister_cover.html">The Dream Twister</a></td>
+		<td>Alex Crosse</td>
+	</tr>
+	<tr>
+		<td>2004</td>
+		<td><a href="fanzines/pragmaclast/Pragmaclast_cover.html">Pragmaclast</a></td>
+		<td>James P. Barrett</td>
+	</tr>
+	<tr>
+		<td>Dec 2009</td>
+		<td><a href="fanzines/danger_dreams/danger-dreams-at-work.pdf">Danger: Dreams at Work</a>
+		<span style="font-variant: small-caps; font-size: 75%;">Warning Very Large PDF (~35MB). It's probably a good idea to download it</span></td>
+		<td>Akbaralli Kapasi</td>
+	</tr>
+</table>
+
+		<!--include "stubs/footer.html"-->
+	</body>
+</html>
+
