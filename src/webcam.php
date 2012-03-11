5c64f4761ba4026e5cf23c8d114783af49e2f1be
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-03-11 11:46:06 +0000

    [MISC] Add a self-refreshing webcam page

---
+++ src/webcam.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="refresh" content="10" />
+		<!--include "stubs/headers.html"-->
+	</head>
+	<body class="no-nav">
+		<h1>Webcam</h1>
+
+		<p style="margin: 1em 0; text-align: center;">
+			<a href="<!--SRVROOT-->/">Back to ICSF Website</a>
+		</p>
+
+		<img style="display: block; width: 95%; max-width: 640px; max-height: 480px; margin: 0 auto;"
+		src="<!--SRVROOT-->/webcam.jpeg" alt="Library Webcam" />
+
+		<p style="margin: 1em 0; text-align: center;">
+			This page will automatically reload every 10 seconds<br />
+			<a href="<!--SRVROOT-->/">Back to ICSF Website</a>
+		</p>
+
+		<!--include "stubs/footer.html"-->
+	</body>
+</html>
+
