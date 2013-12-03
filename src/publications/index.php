d8f1fafc7d04233aca8c584affea6210c6cbbd98
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2013-12-03 18:28:06 +0000

    Fix publication links

--- src/publications/index.php
+++ src/publications/index.php
@@ -14,12 +14,12 @@
 		</nav>
 
 		<h2>Fanzines</h2>
-		<p>Latest Fanzine: &lsquot;<a href="fanzines/danger_dreams/danger-dreams-at-work.pdf">Danger: Dreams at Work</a>&rsquot; (Dec 2009)</p>
+		<p>Latest Fanzine: &lsquo;<a href="<!--SRVROOT-->/fanzines/danger-dreams-at-work.pdf">Danger: Dreams at Work</a>&rsquo; (Dec 2009)</p>
 		<p>
 			The ICSF Fanzines are (usually) annual magazines produced by
 			members of the society (fanzine=magazine produced by fans).
 			They have contained anything from fiction, artwork, and poems,
-			to reviews and essays of sf/fantasy-related topics.
+			to reviews and essays of sf/old/library/fantasy-related topics.
 		</p>
 		<p>
 			As far as we can tell, the first fanzines of the Science Fiction
@@ -52,67 +52,67 @@
 	</tr>
 	<tr>
 		<td>1981-1982</td>
-		<td><a href="fanzines/nme3/nme3_cover.html">NME 3</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/nme3/nme3_cover.html">NME 3</a></td>
 		<td>Elise Pechersky</td>
 	</tr>
 	<tr>
 		<td>1984-ish?</td>
-		<td><a href="fanzines/section10/section10_cover.html">Section 10, the alternative S.F. Soc magazine</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/section10/section10_cover.html">Section 10, the alternative S.F. Soc magazine</a></td>
 		<td>Guy Riddihough</td>
 	</tr>
 	<tr>
 		<td nowrap ="nowrap">October 1985</td>
-		<td><a href="fanzines/whjc/whjc_cover.html">WHJC</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/whjc/whjc_cover.html">WHJC</a></td>
 		<td>Dave Clements</td>
 	</tr>
 	<tr>
 		<td>1989/90</td>
-		<td><a href="fanzines/we/we_cover.html">The Waking Edge</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/we/we_cover.html">The Waking Edge</a></td>
 		<td>Amanda Baker</td>
 	</tr>	
 	<tr>
 		<td>1990/91</td>
-		<td><a href="fanzines/the_edge/the_edge_cover.html">The Edge</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/the_edge/the_edge_cover.html">The Edge</a></td>
 		<td>Paul Davison</td>
 	</tr>	
 	<tr>
 		<td>October 1991</td>
-		<td><a href="fanzines/et1/et1_cover.html">Edge of Tomorrow</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/et1/et1_cover.html">Edge of Tomorrow</a></td>
 		<td>Alex McLintock</td>
 	</tr>
 	<tr>
 		<td>1992</td>
-		<td><a href="fanzines/et2/et2_cover.html">The Edge of Tomorrow II</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/et2/et2_cover.html">The Edge of Tomorrow II</a></td>
 		<td>Richard Yagodich</td>
 	</tr>
 	<tr>
 		<td>1993</td>
-		<td><a href="fanzines/xenomorph/xenomorph_cover.html">Xenomorph</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/xenomorph/xenomorph_cover.html">Xenomorph</a></td>
 		<td>jk</td>
 	</tr>
 	<tr>
 		<td>June 1994</td>
-		<td><a href="fanzines/arcs/arcs_cover.html">Arcs</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/arcs/arcs_cover.html">Arcs</a></td>
 		<td>Luke Gietzen and jk</td>
 	</tr>
 	<tr>
 		<td>June 1995</td>
-		<td><a href="fanzines/parsec/parsec_cover.html">Parsec</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/parsec/parsec_cover.html">Parsec</a></td>
 		<td nowrap ="nowrap">Stian "naSti" Ingebrethsen</td>
 	</tr>
 	<tr>
 		<td>June 1996</td>
-		<td><a href="fanzines/metathesis/metathesis_cover.html">Metathesis</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/metathesis/metathesis_cover.html">Metathesis</a></td>
 		<td>Gidon 'Sandals' Moont</td>
 	</tr>
 	<tr>
 		<td>June 1997</td>
-		<td><a href="fanzines/paradigm/paradigm_cover.html">Paradigm</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/paradigm/paradigm_cover.html">Paradigm</a></td>
 		<td>Michael 'karne' Wright</td>
 	</tr>
 	<tr>
 		<td>June 1998</td>
-		<td><a href="fanzines/synergy/synergy_cover.html">Synergy</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/synergy/synergy_cover.html">Synergy</a></td>
 		<td>Liz 'ShadowCat' Hodson</td>
 	</tr>
 	<tr>
@@ -122,7 +122,7 @@
 	</tr>
 	<tr>
 		<td>Feb 2000</td>
-		<td><a href="fanzines/axiom/axiom_cover.html">Axiom</a>	</td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/axiom/axiom_cover.html">Axiom</a>	</td>
 		<td>Lloyd 'Hack' Kilford and <br />Lorna Robinson</td>
 	</tr>
 	<tr>
@@ -132,23 +132,22 @@
 	</tr>
 	<tr>
 		<td>2002</td>
-		<td><a href="fanzines/perihelion/Perihelion_cover.html">Perihelion</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/perihelion/Perihelion_cover.html">Perihelion</a></td>
 		<td>Alex Crosse</td>
 	</tr>
 	<tr>
 		<td>2003</td>
-		<td><a href="fanzines/dream_twister/The Dream Twister_cover.html">The Dream Twister</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/dream_twister/The Dream Twister_cover.html">The Dream Twister</a></td>
 		<td>Alex Crosse</td>
 	</tr>
 	<tr>
 		<td>2004</td>
-		<td><a href="fanzines/pragmaclast/Pragmaclast_cover.html">Pragmaclast</a></td>
+		<td><a href="<!--SRVROOT-->/old/library/fanzines/pragmaclast/Pragmaclast_cover.html">Pragmaclast</a></td>
 		<td>James P. Barrett</td>
 	</tr>
 	<tr>
 		<td>Dec 2009</td>
-		<td><a href="fanzines/danger_dreams/danger-dreams-at-work.pdf">Danger: Dreams at Work</a>
-		<span style="font-variant: small-caps; font-size: 75%;">Warning Very Large PDF (~35MB). It's probably a good idea to download it</span></td>
+		<td><a href="<!--SRVROOT-->/fanzines/danger-dreams-at-work.pdf">Danger: Dreams at Work</a></td>
 		<td>Akbaralli Kapasi</td>
 	</tr>
 </table>
