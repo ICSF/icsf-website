f910fe728ab91997a4696c269cd6e31f0d357350
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2013-12-04 20:47:13 +0000

    Make Tube line colours clearer on Picocon page

--- src/picocon/index.php
+++ src/picocon/index.php
@@ -145,9 +145,9 @@
 				</p>
 				<p>
 					Both stations are served by the
-					<span class="tube-line" style="background-color: rgba(255,209,0,0.5);">Circle</span>,
-					<span class="tube-line" style="background-color: rgba(0, 114, 41, 0.5);">District</span>, and
-					<span class="tube-line" style="background-color: rgba(0, 25, 168, 0.5);">Piccadilly</span>
+					<span class="tube-line" style="background-color: rgba(255,209,0,0.8);">Circle</span>,
+					<span class="tube-line" style="background-color: rgba(0, 114, 41, 0.8);">District</span>, and
+					<span class="tube-line" style="background-color: rgba(0, 25, 168, 0.8);">Piccadilly</span>
 					lines.
 				</p>
 				<p>
