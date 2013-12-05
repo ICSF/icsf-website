ddc1299b180e22a2977b83d567a177432f7dc318
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2013-12-05 21:18:39 +0000

    Add badic validation logic

--- bin/validator/html.php
+++ bin/validator/html.php
@@ -2,27 +2,115 @@
 
 class HtmlValidator extends Validator
 {
+	/** @var array */
 	public static $tags = array();
-
+	/** @var array */
 	protected $tagStack = array();
 
 	protected function run($content)
 	{
-		$regex = '/\<(?<close>\/?)(?<tag>[a-z:!]+)(?<attrs> [^\/>]+)?(?<selfclose>\/?)\>/smU';
+		$regex = '/\<(?<close>\/?)(?<tag>[a-z:!]+)(?<attrs>[^>]+)?(?<selfclose>\/?)\>/smU';
 		$offset = 0;
+		$match = array();
+
 		while (preg_match($regex, $content, $match, PREG_OFFSET_CAPTURE, $offset))
 		{
-			$offset  = $match[0][1] + strlen($match[0][0]);
+			$offset  = $match[0][1] + mb_strlen($match[0][0]);
 
 			$match = array(
+				'line' => substr_count($content, "\n", 0, $offset),
 				'tag' => $match['tag'][0],
 				'attrs' => $match['attrs'][0],
 				'close' => !empty($match['close'][0]),
 				'selfclose' => !empty($match['selfclose'][0])
 			);
-			print_r($match);
+
+			if (!array_key_exists($match['tag'], self::$tags))
+			{
+				$this->error($match['line'], false, "Dropping unrecognised tag " + $match['tag']);
+				continue;
+			}
+
+			if ($match['close'])
+			{
+				$this->close($match);
+				continue;
+			}
+
+			$this->open($match);
+
+			$match['attrs'] = $this->parseAttributes($match);
+			$missing = self::$tags[$match['tag']]->getMissingAbbributes($match['attrs']);
+
+			foreach ($missing as $missed)
+			{
+				$this->error($match['line'], false, sprintf('Tag %s missing attribute %s', $match['tag'], $missed));
+			}
 		}
 	}
+
+	protected function open(array $match)
+	{
+		$current = end($this->tagStack);
+
+		if ($current === false)
+		{
+			if (!$match['selfclose'])
+			{
+				array_push($this->tagStack, $match['tag']);
+			}
+			return;
+		}
+
+		$current = self::$tags[$current];
+
+		if ($current->isChildPermitted($match['tag']))
+		{
+			$this->error($match['line'], false, sprintf('Tag %s may not be a child of %s', $match['tag'], $current->getName()));
+		}
+
+		if (!$match['selfclose'])
+		{
+			array_push($this->tagStack, $match['tag']);
+		}
+	}
+
+	protected function close(array $match)
+	{
+		$current = end($this->tagStack);
+
+		if ($current === false)
+		{
+			$this->error($match['line'], false, sprintf('Tag %s closed when no tags are open', $match['tag']));
+			return;
+		}
+
+		if ($match['tag'] !== $current)
+		{
+			$this->error($match['line'], false, sprintf('Tag %s attempting to close %s', $match['tag'], $current));
+
+			// If the closing tag is a container for the last opened,
+			// the close propigates up the stack
+			if (array_key_exists($current, self::$tags[$match['tag']]->children))
+			{
+				array_pop($this->tagStack);
+				$this->close($match);
+			}
+		}
+		else
+		{
+			array_pop($this->tagStack);
+		}
+	}
+
+	public function parseAttributes(array $match)
+	{
+		$regex = '/([a-z\-]+)=(["\'])([^\2]+)\2/smU';
+		preg_match_all($regex, $content, $match);
+
+		var_dump($match);
+	}
+
 }
 
 $head  = array('title', 'link', 'meta', 'script', 'style');
@@ -75,17 +163,22 @@ unset($head, $sec, $block, $bflow, $flow, $exts);
 class HtmlTag
 {
 	/** @var string */
-	public $name;
+	protected $name;
 	/** @var array */
-	public $children;
+	protected $children;
 	/** @var array */
-	public $attrs;
+	protected $attrs;
 
 	public function __construct($name, array $children = array(), array $attrs = array())
 	{
 		$this->name = $name;
 		$this->children = array_flip($children);
 		$this->attrs = array_flip($attrs);
+	}
+
+	public function getName()
+	{
+		return $this->name;
 	}
 
 	public function isChildPermitted($child)
