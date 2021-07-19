4fec1be01ecaa1fcad87efa2d749ad6422779c51
Jakub Dranczewski <domdlapsa@gmail.com>
2021-07-19 22:43:21 +0200

    Styles, nav, and homepage updated
    Also Steampunk

--- src/index.php
+++ src/index.php
@@ -23,22 +23,35 @@
 				href="<!--SRVROOT-->/webcam.php">Webcam Page</a>
 		</div>
 
+		<div id="discord-widget">
+			<img id="discord-logo" src="resources/discord_logo.png" alt="Discord server logo" />
+			<a href="https://discord.gg/Wy7VXpM" target="_blank" title="Join our Discord server" id="discord-join">Join</a>
+			Sci Fi & Fantasy Society<br>
+			<div id="discord-indicator"></div><span><?php echo json_decode(file_get_contents('https://discord.com/api/guilds/430437751603724319/widget.json'), true)['presence_count'];?> members online</span>
+		</div>
+
+		<p>
+			<strong>In person events may not happen anytime soon, but we are still active! Join our
+ 			Discord server to chat about all the stuff we usually chat about (in text channels 24/7 and
+			in voice channels on Wednesdays), games nights on Fridays, and any other events we (or you)
+			can think about!</strong>
+		</p>
+
 		<p>
 			ICSF is the centre for science fiction, fantasy and horror fans at
 			Imperial College. We run a library of over 9000 books and 1800 DVDs
 			and videos from all corners of the genre. Whether your interest is
-			Pratchett's 'Discworld', 'The Sandman' graphic novels or 'Stargate'
-			this is an ideal way to indulge it! And a great place to pick up new
-			and exciting obsessions too!
+			Pratchett's <i>Discworld</i>, <i>The Sandman</i> graphic novels, or
+			<i>Stargate</i>, this is an ideal way to indulge it! It's a great
+			place to pick up new and exciting obsessions too!
 		</p>
 		<p>
-			The social side of the society is equally active - the
+			The social side of the society is equally active &ndash; the
 			<a href="<!--SRVROOT-->/library/">library</a> is open every
 			lunchtime on weekdays and we have regular video showings
 			(often weird or esoteric stuff you should have seen but won't
-			have).
-			Barnights are on Thursdays, these are often frequented by past
-			members of the society.
+			have). Film night are on Fridays, and we have large themed events
+			for Halloween and Christmas.
 			We also run events like mass outings to the cinema and end
 			of term dinners; you can see what is coming up on the
 			<a href="<!--SRVROOT-->/events/">events page</a>.
@@ -54,11 +67,20 @@
 			interested in SF or fantasy.
 			Interested?  We can be found in our purpose built library in the
 			West Wing Basement of Beit Quad (the Students Union building),
-			opposite Stoic TV. Click for <a
+			opposite IC Radio. Click for <a
 			href="<!--SRVROOT-->/library/getting-to.html">directions to the library</a>.
+		</p>
+
+		<p>
+			You can also find us on social media:
+			<br>
+			<a href="https://www.facebook.com/groups/ICSF.Imperial/" target="_blank">Facebook</a>
+			<br>
+			<a href="https://twitter.com/Imperial_SciFi" target="_blank">Twitter</a>
+			<br>
+			<a href="https://www.instagram.com/imperial_scifi/" target="_blank">Instagram</a>
 		</p>
 
 		<!--include "stubs/footer.html"-->
 	</body>
 </html>
-
